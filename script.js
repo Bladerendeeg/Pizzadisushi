18f7049c4d7b38cdb0e6510706675766f1bac664
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-23 10:53:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const pizzaRecept = function () {
+    console.log("Deeg maken");
+    console.log("Deeg rollen");
+    console.log("Tomatensaus erop");
+    console.log("kaas erop");
+
+}
+
+const sushiRecept = function () {
+    console.log("Lekkerrr");
+}
+sushiRecept();
+sushiRecept();
+sushiRecept();
+pizzaRecept();
+pizzaRecept();
